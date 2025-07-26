a88e8c0c57064bd965169d7c2717dffa6d8754ac
Suresh Pattanaik <sureshpattanaik14920@gmail.com>
2025-07-26 16:43:11 +0530

    commit

---
+++ TechAcademy_java_selenium_course_1/src/day5/ExceptionInArray.java
@@ -0,0 +1,34 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ExceptionInArray {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		try(sc)
+		{
+			System.out.println("Enter the size of Array :");
+			int n=sc.nextInt();
+			
+			int [] a=new int[n];
+			
+			System.out.println("Enter the Element of Array :");
+			
+			for(int i=0;i<n;i++)
+			{
+				 a[i]=sc.nextInt();
+			}
+			System.out.println(a[n]);
+		}
+		catch(Exception e)
+		{
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ TechAcademy_java_selenium_course_1/src/day6/ExceptionHandleDemo.java
@@ -0,0 +1,32 @@
+package day6;
+
+import java.util.Scanner;
+
+public class ExceptionHandleDemo {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the 1st Integer");
+		int a=sc.nextInt();
+		System.out.println("Enter the 2nd Integer");
+		int b=sc.nextInt();
+		try
+		{
+			int c=a/b;
+			System.out.println("Result will be : "+c);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally {
+			sc.close();
+			System.out.println("Now we are reached the finally block");
+		}
+		
+		
+
+	}
+
+}
